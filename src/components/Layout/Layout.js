0163918635cc11a392d2aa4940c3b47473ddc550
cher <cher@chers-MacBook-Pro.local>
2025-02-26 03:42:05 +0700

    services page

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,27 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import Header from '../UI/Header/Header';
 import Footer from '../UI/Footer/Footer';
-import Sections from '../sections/Sections';
+import Home from '../sections/Home/Home.js';
+import About from '../sections/About/About.js';
+import Contact from '../sections/Contact/Contact.js';
+import Service from '../sections/Service/Service.js';
 
 const Layout = () => {
   return (
-    <Fragment>
+    <>
       <Header />
       <main>
-        <Sections />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/About" exact component={About} />
+          <Route path="/Contact" component={Contact} />
+          <Route path="/Service" component={Service} />
+        </Switch>
       </main>
       <Footer />
-    </Fragment>
+    </>
   );
 };
 
