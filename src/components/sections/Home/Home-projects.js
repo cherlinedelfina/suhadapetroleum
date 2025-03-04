13ad323434697ab8c608f7027e82b6266902be98
Clarisse Tandra <ctan0170@student.monash.edu>
2025-03-04 19:47:56 +1100

    Fixed the description of the company in homepage

--- src/components/sections/Home/Home-projects.js
+++ src/components/sections/Home/Home-projects.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Section from '../../../HOC/Section';
 import projects_bg from '../../../assets/img/projects_bg.jpg';
-import '../../../scss/partials/home/_homeproject.scss';
 import image1 from '../../../assets/img/project1.jpg'; 
 import image2 from '../../../assets/img/project2.jpg';
+import '../../../scss/partials/home/_homeproject.scss'; 
 
 const HomeProject = () => {
   return (
@@ -11,8 +11,7 @@ const HomeProject = () => {
       <div className="bg-container" style={{ position: 'relative' }}> 
         <img src={projects_bg} alt="projects background" className="projects_bg" />
 
-        <div className="two-column-container" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
-          {/* Left Column */}
+        <div className="two-column-container">
           <div className="left-column" style={{ flex: '1', marginRight: '20px', backgroundColor: 'white', padding: '20px', borderRadius: '5px', boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)' }}>
             <div className="inner-column">
               <img src={image1} alt="NOV Reciprocating Pump Model 1759 Package" className="column-image" />
@@ -22,7 +21,6 @@ const HomeProject = () => {
             </div>
           </div>
 
-          {/* Right Column */}
           <div className="right-column" style={{ flex: '1', backgroundColor: 'white', padding: '20px', borderRadius: '5px', boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)' }}>
             <div className="inner-column">
               <img src={image2} alt="ARROW Gas Engine Model C-106" className="column-image" />
