32f0a2e91a0113a181286181bdf02f3d97819a2b
Clarisse Tandra <ctan0170@student.monash.edu>
2025-03-20 14:28:11 +1100

    Made it more mobile responsive

--- src/components/sections/Home/Home-about.js
+++ src/components/sections/Home/Home-about.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Section from '../../../HOC/Section';
 import solidFeedingImage from '../../../assets/img/solid_feeding.jpg';
+import '../../../scss/partials/home/_homeabout.scss';
 
 const About = () => {
   const history = useHistory();
@@ -10,20 +11,18 @@ const About = () => {
     <Section id='about'>
       <div className='container pt-2 pb-5'>
         <div className="two-column-container">
-          <div className='text-column'>
+          <div className='about-text-column'>
             <div className='main-header'>
-              <h3 className='main-title '>PT SUHADA PETROLEUM</h3>
+              <h3 className='main-title'>PT SUHADA PETROLEUM</h3>
             </div>
             <h6 className='main-subtitle'>
               Established in 2000, is a company which focuses on trading and supporting mechanical engineering, electrical, instrumentation, and maintenance services, especially for oil and gas industries.
             </h6>
-          
-          
             <button className='btn' onClick={() => history.push('/About')}>
               FIND OUT MORE
             </button>
           </div>
-          <div className="image-column">
+          <div className="about-image-column">
             <img src={solidFeedingImage} alt="About PT Suhada Petroleum" className="solidFeedingImage" />
           </div>
         </div>
--- src/components/sections/Home/Home-welcome.js
+++ src/components/sections/Home/Home-welcome.js
@@ -4,16 +4,16 @@ import bg1 from '../../../assets/img/bg1.jpg';
 import bg2 from '../../../assets/img/bg2.jpg';
 import bg3 from '../../../assets/img/bg3.jpg';
 import bg4 from '../../../assets/img/bg4.jpg';
+import '../../../scss/partials/home/_homewelcome.scss'; 
 
 const Home = () => {
-  const [slideIndex, setSlideIndex] = useState(1); 
+  const [slideIndex, setSlideIndex] = useState(1);
 
   const plusSlides = (n) => {
-    
     setSlideIndex(prevIndex => {
       let newIndex = prevIndex + n;
-      if (newIndex > 4) newIndex = 1; 
-      if (newIndex < 1) newIndex = 4; 
+      if (newIndex > 4) newIndex = 1;
+      if (newIndex < 1) newIndex = 4;
       return newIndex;
     });
   };
@@ -25,74 +25,57 @@ const Home = () => {
   const showSlides = (n) => {
     const slides = document.getElementsByClassName("mySlides");
     const dots = document.getElementsByClassName("dot");
-    
-    
-    if (n > slides.length) { n = 1; }
-    if (n < 1) { n = slides.length; }
 
-    
+    if (n > slides.length) n = 1;
+    if (n < 1) n = slides.length;
+
     for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+      slides[i].style.display = "none";
     }
-   
+
     for (let i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
     }
-    
-    slides[n - 1].style.display = "block";  
+
+    slides[n - 1].style.display = "block";
     dots[n - 1].className += " active";
   };
 
   useEffect(() => {
-    const slides = document.getElementsByClassName("mySlides");
-    const dots = document.getElementsByClassName("dot");
-
-    
     showSlides(slideIndex);
-
     const timer = setInterval(() => {
-      plusSlides(1); 
-    }, 5000); 
-
-    return () => clearInterval(timer); 
-  }, [slideIndex]); 
+      plusSlides(1);
+    }, 5000);
+    return () => clearInterval(timer);
+  }, [slideIndex]);
 
   return (
     <Section id='home'>
-      <div>
+      <div className="slideshow-container">
+        <div className="mySlides">
+          <img src={bg1} alt="Slide 1" />
+        </div>
+        <div className="mySlides">
+          <img src={bg2} alt="Slide 2" />
+        </div>
+        <div className="mySlides">
+          <img src={bg3} alt="Slide 3" />
+        </div>
+        <div className="mySlides">
+          <img src={bg4} alt="Slide 4" />
+        </div>
+
         
+        <button className="prev" onClick={() => plusSlides(-1)}>&#10094;</button>
+        <button className="next" onClick={() => plusSlides(1)}>&#10095;</button>
+      </div>
 
-  
-        <div className="slideshow-container">
-      
-          <div className="mySlides">
-            <img src={bg1} style={{ width: "100%", height: "calc(100vh - 100px)", objectFit: "cover" }} alt="Image 1" />
-          </div>
-
-          <div className="mySlides">
-            <img src={bg2} style={{ width: "100%", height: "calc(100vh - 100px)", objectFit: "cover" }} alt="Image 2" />
-          </div>
-
-          <div className="mySlides">
-            <img src={bg3} style={{ width: "100%", height: "calc(100vh - 100px)", objectFit: "cover" }} alt="Image 3" />
-          </div>
-
-          <div className="mySlides">
-            <img src={bg4} style={{ width: "100%", height: "calc(100vh - 100px)", objectFit: "cover" }} alt="Image 4" />
-          </div>
-
-         
-          <a className="prev" onClick={() => plusSlides(-1)}>&#10094;</a>
-          <a className="next" onClick={() => plusSlides(1)}>&#10095;</a>
-        </div>
-        <br />
-
-        <div style={{ textAlign: "center" }}>
-          <span className="dot" onClick={() => currentSlide(1)}></span>
-          <span className="dot" onClick={() => currentSlide(2)}></span>
-          <span className="dot" onClick={() => currentSlide(3)}></span>
-          <span className="dot" onClick={() => currentSlide(4)}></span>
-        </div>
+     
+      <div className="dots-container">
+        <span className="dot" onClick={() => currentSlide(1)}></span>
+        <span className="dot" onClick={() => currentSlide(2)}></span>
+        <span className="dot" onClick={() => currentSlide(3)}></span>
+        <span className="dot" onClick={() => currentSlide(4)}></span>
       </div>
     </Section>
   );
